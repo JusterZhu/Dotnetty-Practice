870069466baaeed63c4d54e359e76bc0e14f783e
朱震 <zhuzhen723723@outlook.com>
2021-02-21 13:27:18 +0800

    提交Part1 和 Part2

---
+++ Part2Socket基础原理/src/MySocketServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MySocketServer
+{
+    class Program
+    {
+        //服务端口号
+        private const int PORT = 8888;
+        private const string message = "Hello world,message to client!";
+
+        static void Main(string[] args)
+        {
+            Socket server = null;
+            try
+            {
+                var ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT);
+                //初始化socket
+                server = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                server.ReceiveBufferSize = 1024;
+                server.SendBufferSize = 1024;
+                //NoDelay:true 直接将消息发出，而不是等待发送字符累计到SendBufferSize上限时再发送
+                /*
+                 * NoDelay:false 将要发送的消息先缓存到发送字符池中，
+                 * 等到累计到SendBufferSize（缓冲池）上限时一起发送出去.
+                 * 如果一直没有到达上限，则到等待时间之后自动发出不再等待。
+                */
+                server.NoDelay = true;
+                server.SendTimeout = 1000 * 3;
+                server.ReceiveTimeout = 1000 * 3;
+                //将socket绑定当前ip
+                server.Bind(ip);
+                //监听，监听队列最大的长度为1024
+                server.Listen(1024);
+
+                Socket clientSocket = null;
+                byte[] receiveBytes = new byte[1024];
+                //等待客户端的连接
+                clientSocket = server.Accept();
+                //接收客户端发送的消息
+                while (clientSocket.Receive(receiveBytes) > 0)
+                {
+                    //打印客户端消息，并回复
+                    string content = Encoding.UTF8.GetString(receiveBytes);
+                    Console.WriteLine($"[{ DateTime.Now }] Receive message form client : { content } .");
+                    clientSocket.Send(Encoding.UTF8.GetBytes($"[{ DateTime.Now }] { message }"));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //关闭服务
+                if (server != null)
+                {
+                    Console.WriteLine("The server close.");
+                    server.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Part2Socket基础原理/src/SocketDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MySocketClient
+{
+    class Program
+    {
+        //服务提供的端口号
+        private const int PORT = 8888;
+        private const string message = "Hello world,message to server!";
+
+        static void Main(string[] args)
+        {
+            Socket client = null;
+            try
+            {
+                //初始化socket client
+                client = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                client.ReceiveBufferSize = 1024;
+                client.SendBufferSize = 1024;
+                client.NoDelay = true;
+                client.SendTimeout = 1000 * 3;
+                client.ReceiveTimeout = 1000 * 3;
+                //连接服务端
+                client.Connect(IPAddress.Parse("127.0.0.1"), PORT);
+                //向服务端发送消息
+                byte[] sendBytes = Encoding.UTF8.GetBytes(message);
+                client.Send(sendBytes);
+                byte[] receiveBytes = new byte[1024];
+                while (client.Receive(receiveBytes) > 0)
+                {
+                    string content = Encoding.UTF8.GetString(receiveBytes);
+                    Console.WriteLine($"[{ DateTime.Now }] Receive message form server : { content } .");
+                    client.Send(sendBytes);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                //关闭客户端
+                if (client != null)
+                {
+                    Console.WriteLine("The client close.");
+                    client.Close();
+                }
+            }
+            Console.Read();
+        }
+    }
+}
